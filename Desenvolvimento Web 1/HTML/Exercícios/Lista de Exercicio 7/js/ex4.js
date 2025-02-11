94dabf68b4f323cddd7be8c8ae2d101c58f197a3
gabecamargo <gabrielisaway@gmail.com>
2025-02-10 21:15:54 -0300

    Delete Desenvolvimento Web 1/HTML directory

--- Desenvolvimento Web 1/HTML/Exercícios/Lista de Exercicio 7/js/ex4.js
+++
@@ -1,9 +0,0 @@
-function exer4() {   
-    var nome = "Ana Maria";
-
-    var novoparagrafo = document.createElement("p");
-
-    novoparagrafo.innertext = nome;
-
-    document.getElementById("saida").appendChild(novoparagrafo);
-  }
